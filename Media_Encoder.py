8980216b0206ac48e7e16e1bc2cc91f4d41eab88
himanshu-hs3773 <hs3773@nyu.edu>
2021-03-01 08:58:03 -0800

    Media Encoder to find minimum continous sum of processing time

--- Media_Encoder.py
+++ Media_Encoder.py
@@ -1,32 +1,28 @@
-fileSizes = [4, 8, 6, 12]
+def optimalSum(fileSizes: [int]) -> int:
 
-fileSizes = sorted(fileSizes, reverse=True)
+    fileSizes = sorted(fileSizes, reverse=True)
+    sums_arr = [0] * (len(fileSizes)+1)
+    temp_sum = float('-inf')
+    start = 0
 
-print("a thing queue: ", fileSizes, "\n")
+    if not len(fileSizes):
+        return -1
+    while len(fileSizes) > 1:
 
-sums_arr = [0] * (len(fileSizes)+1)
-temp_sum = float('-inf')
-queue_size = len(fileSizes) + 1
-start = 0
+        if temp_sum < fileSizes[-2]:
+            sums_arr[start + 1] += sums_arr[start] + fileSizes[-1]
+            fileSizes.pop()
+            temp_sum = sums_arr[start + 1]
+            start += 1
 
-if not len(fileSizes):
-    print(sum(sums_arr[2:]))
-while len(fileSizes) > 1:
+        else:
+            sums_arr[start + 1] += fileSizes[-1] + fileSizes[-2]
+            fileSizes.pop()
+            fileSizes.pop()
+            temp_sum = sums_arr[start + 1]
+            start += 1
 
-    if temp_sum < fileSizes[-2]:
-        sums_arr[start + 1] += sums_arr[start] + fileSizes[-1]
-        fileSizes.pop()
-        temp_sum = sums_arr[start + 1]
-        start += 1
+    if len(fileSizes) == 1:
+        sums_arr[start + 1] += fileSizes[0] + sums_arr[start]
 
-    else:
-        sums_arr[start + 1] += fileSizes[-1] + fileSizes[-2]
-        fileSizes.pop()
-        fileSizes.pop()
-        temp_sum = sums_arr[start + 1]
-        start += 1
-
-if len(fileSizes) == 1:
-    sums_arr[start + 1] += fileSizes[0] + sums_arr[start]
-
-print(sum(sums_arr[2:]))
+    return sum(sums_arr[2:])
